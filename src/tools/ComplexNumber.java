b2d261e68a0e8537417bd33515bf739c64276c32
DELL <DELL@DESKTOP-IA4G1IT>
2023-09-21 11:56:34 -0600

    se agrego  los maetodos de suma, resta, multiplicacion, division , toN, angulo, modulo

---
+++ src/tools/ComplexNumber.java
@@ -0,0 +1,90 @@
+package tools;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DELL
+ */
+public class ComplexNumber {
+    
+    
+    private float a; 
+    private float b;
+    
+    public ComplexNumber(){
+    }
+
+    public ComplexNumber(float a, float b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void setA(float a) {
+        this.a = a;
+    }
+
+    public void setB(float b) {
+        this.b = b;
+    }
+
+    public float getA() {
+        return a;
+    }
+
+    public float getB() {
+        return b;
+    }
+    
+  
+    public ComplexNumber conj(){
+        return new ComplexNumber(this.a,  -this.b);
+    }
+    
+    public static ComplexNumber conj(ComplexNumber z){
+        return new ComplexNumber(z.a,  -z.b);
+    }
+    
+    public ComplexNumber add(ComplexNumber z){
+        return new ComplexNumber(this.a  + z.a, this.b + z.b );
+    }
+    
+    public ComplexNumber subs(ComplexNumber z){
+        return new ComplexNumber(this.a - z.a, this.b + z.b);
+    }
+    
+    public ComplexNumber mult (ComplexNumber z){
+        return new ComplexNumber(
+                this.a * z.a - this.b * z.b,
+                this.a * z.b + this.b * z.a);
+    }
+    
+    public ComplexNumber div(ComplexNumber z){
+        ComplexNumber aux = mult(conj(z) );
+        float den = z.a * z.a+ z.b + z.b;
+        return  new ComplexNumber(aux.a/den, aux.b/den);
+    }
+    
+    public float mod(){
+        return (float) Math.sqrt(this.a + this.a * this.b * this.b);
+    }
+    public static float mod(ComplexNumber z){
+        return (float) Math.sqrt(z.a + z.a * z.b * z.b);
+    }
+    public  float angle(){
+        return (float) Math.atan2(this.b, this.a);
+    }
+    
+    public static float angle(ComplexNumber z ){
+     return (float)  Math.atan2(z.b, z.a);
+    }
+        
+    
+    
+
+
+
+}
